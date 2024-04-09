5313eec0be7d3918fba134228232fab0d80f5a90
poncest <steven_ponce@yahoo.com>
2024-04-09 05:44:28 -0400

    day 09

---
+++ 2024/day_09/2024_day_09.R
@@ -0,0 +1,321 @@
+
+## Challenge: #30DayChartChallenge 2024 day 09
+## Topic:     Distributions | major/minor
+## Author:    Steven Ponce
+## Date:      2024-04-09
+
+## Data:      US Accidents (2016 - 2023) (via Sobhan Moosavi)
+##            A Countrywide Traffic Accident Dataset (2016 - 2023)
+## Link:      https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents/data
+
+## Citation:   
+#' Sobhan Moosavi. (2023). US Accidents (2016 - 2023) [Data set]. 
+#' Kaggle. https://doi.org/10.34740/KAGGLE/DS/199387
+
+
+## 1. LOAD PACKAGES & SETUP ----  
+pacman::p_load(
+  tidyverse,   # Easily Install and Load the 'Tidyverse'
+  ggtext,      # Improved Text Rendering Support for 'ggplot2'
+  showtext,    # Using Fonts More Easily in R Graphs
+  janitor,     # Simple Tools for Examining and Cleaning Dirty Data
+  skimr,       # Compact and Flexible Summaries of Data
+  scales,      # Scale Functions for Visualization
+  lubridate,   # Make Dealing with Dates a Little Easier
+  glue,        # Interpreted String Literals
+  MetBrewer,   # Color Palettes Inspired by Works at the Metropolitan Museum of Art
+  MoMAColors,  # Color Palettes Inspired by Artwork at the Museum of Modern Art in New York City
+  ggbeeswarm,  # Categorical Scatter (Violin Point) Plots
+  data.table   # Extension of `data.frame`
+)
+
+
+### |- figure size ---- 
+camcorder::gg_record( 
+  dir    = here::here("temp_plots"), 
+  device = "png",
+  width  = 6,
+  height = 5,
+  units  = "in",
+  dpi    = 320)
+
+### |- resolution ---- 
+showtext_opts(dpi = 320, regular.wt = 300, bold.wt = 800)
+
+
+## 2. READ IN THE DATA ----
+raw_data <- data.table::fread(here::here("2024/data/US_Accidents_March23.csv")) |>
+  clean_names() |>
+  # Drop unwanted columns
+  select(
+    -c(end_time:street, zipcode),
+    -c(airport_code:nautical_twilight),
+  ) |>
+  glimpse()
+
+
+#' NOTE
+#' If interested, download the data here: https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents/data
+#' File too big to push to GitHub.
+#' 
+
+## 3. EXAMINING THE DATA ----
+glimpse(raw_data)
+skim(raw_data)
+colnames(raw_data)
+
+raw_data |> count(state, sort = T)
+raw_data |> count(severity, sort = T)
+
+
+
+## 4. TIDYDATA ----
+
+### |- Tidy ----
+
+plot_data <- raw_data  |> 
+  mutate(
+    year = year(start_time),
+    severity_scale = if_else(severity <= 2, "Minor Accident", "Major Accident")
+  ) |>
+  select(year, severity_scale, state, astronomical_twilight) |>
+  filter(astronomical_twilight != "") |>
+  count(severity_scale, state, astronomical_twilight, name = "count") |>
+  arrange(desc(count))
+
+
+# Housekeeping 
+rm(raw_data)
+gc()
+
+
+# 5. VISUALIZATION ---- 
+
+### |- plot aesthetics ---- 
+bkg_col      <- '#fafafa'
+title_col    <- "gray10"              
+subtitle_col <- "gray20"   
+caption_col  <- "gray20"  
+text_col     <- "gray20"    
+col_palette  <- c('Night' = '#355C7D', 'Day' = '#E76F51')
+
+
+### |-  titles and caption ----
+# icons
+tt <- str_glue("#30DayChartChallenge: { 2024 } Day { 09 } &bull; Source: Kaggle via Sobhan Moosavi<br>")  
+li <- str_glue("<span style='font-family:fa6-brands'>&#xf08c;</span>")  
+gh <- str_glue("<span style='font-family:fa6-brands'>&#xf09b;</span>")
+mn <- str_glue("<span style='font-family:fa6-brands'>&#xf4f6;</span>")
+
+# text
+title_text   <- str_glue("Traffic Accidents in the United States") 
+
+day   <- str_glue("<span style='color:{ col_palette['Day'] }'>**Day**</span>")
+night <- str_glue("<span style='color:{ col_palette['Night'] }'>**Night**</span>")
+
+subtitle_text <- str_glue("Comparison of Major and Minor Car Accidents that Occurs During<br>
+                          {day} or {night} Times from 2016 to 2023")
+
+caption_text <- str_glue("{tt} {li} stevenponce &bull; {mn} @sponce1(graphic.social) &bull; {gh} poncest &bull; #rstats #ggplot2")
+
+### |-  fonts ----
+font_add('fa6-brands', 'fonts/6.4.2/Font Awesome 6 Brands-Regular-400.otf') 
+font_add_google("Poppins", regular.wt = 400, family = "title")       
+font_add_google("Poppins", family = "subtitle")  
+font_add_google("Inter", regular.wt = 400, family = "text")        
+font_add_google("Roboto Condensed", family = "caption")
+showtext_auto(enable = TRUE)  
+
+### |-  plot theme ----
+theme_set(theme_minimal(base_size = 12, base_family = "text"))                
+
+theme_update(
+  plot.title.position   = "plot",
+  plot.caption.position = "plot",
+  legend.position       = "plot",
+  plot.margin           = margin(t = 10, r = 15, b = 0, l = 15),
+  plot.background       = element_rect(fill = bkg_col, color = bkg_col),
+  panel.background      = element_rect(fill = bkg_col, color = bkg_col),
+  axis.title.x          = element_text(margin = margin(10, 0, 0, 0), size = rel(1), color = text_col, family = "text", face = "bold"),
+  axis.title.y          = element_text(margin = margin(0, 10, 0, 0), size = rel(1), color = text_col, family = "text", face = "bold"),
+  axis.text             = element_text(size = rel(.8), color = text_col, family = "text"),
+  axis.line.x           = element_line(color = "black", linewidth = .2),
+  panel.grid.minor.y    = element_blank(),
+  panel.grid.major.y    = element_blank(),
+  panel.grid.minor.x    = element_blank(),
+  panel.grid.major.x    = element_line(linetype = "dotted", linewidth = 0.2, color = "gray"),
+)
+
+
+### |-  final plot ----  
+
+# Reference: https://github.com/eclarke/ggbeeswarm
+
+plot_data |> 
+  ggplot(aes(x = severity_scale, y = count, color = astronomical_twilight)) +
+  
+  # Geoms
+  geom_beeswarm(cex = 2, size = 1.5) +
+  annotate("text",
+           x = "Minor Accident", y = 0, label = "Severity", vjust = -3, hjust = 1.5,
+           size = 3.5, color = text_col, fontface = "bold", family = "text"
+  ) +
+  
+  # Scales
+  scale_x_discrete() +
+  scale_y_log10(labels = comma) +
+  scale_color_manual(values = col_palette) +
+  coord_flip(clip = "off") +
+  
+  # Labs
+  labs(
+    x        = element_blank(),
+    y        = "Count (log scale)",
+    color    = "Astronomical Twilight",
+    title    = title_text,
+    subtitle = subtitle_text,
+    caption  = caption_text,
+  ) +
+  
+  # Theme
+  theme(
+    plot.title      = element_text(
+      size          = rel(1.45),
+      family        = "title",
+      face          = "bold",
+      color         = title_col,
+      lineheight    = 1.1,
+      margin        = margin(t = 5, b = 5)
+    ),
+    plot.subtitle   = element_markdown(
+      size          = rel(.85), 
+      family        = 'subtitle',
+      color         = title_col,
+      lineheight    = 0.85, 
+      margin        = margin(t = 0, b = 0)
+    ),
+    plot.caption    = element_markdown(
+      size          = rel(.65),
+      family        = "caption",
+      color         = caption_col,
+      lineheight    = 0.65,
+      hjust         = 0.5,
+      halign        = 0.5,
+      margin        = margin(t = 10, b = 5)
+    ),
+  )
+
+
+# 6. SESSION INFO ----  
+sessioninfo::session_info(include_base = TRUE) 
+
+# ─ Session info ────────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.3.3 (2024-02-29 ucrt)
+# os       Windows 10 x64 (build 19045)
+# system   x86_64, mingw32
+# ui       RStudio
+# language (EN)
+# collate  English_United States.utf8
+# ctype    English_United States.utf8
+# tz       America/New_York
+# date     2024-04-07
+# rstudio  2023.12.1+402 Ocean Storm (desktop)
+# pandoc   NA
+# 
+# ─ Packages ────────────────────────────────────────────────────────────────────
+# ! package     * version    date (UTC) lib source
+# base        * 4.3.3      2024-02-29 [2] local
+# P base64enc     0.1-3      2015-07-28 [?] CRAN (R 4.3.0)
+# P beeswarm      0.4.0      2021-06-01 [?] CRAN (R 4.3.1)
+# P camcorder     0.1.0      2022-10-03 [?] CRAN (R 4.3.0)
+# cli           3.6.2      2023-12-11 [1] CRAN (R 4.3.3)
+# P colorspace    2.1-0      2023-01-23 [?] CRAN (R 4.3.0)
+# P commonmark    1.9.0      2023-03-17 [?] CRAN (R 4.3.0)
+# P compiler      4.3.3      2024-02-29 [?] local
+# cowplot       1.1.3      2024-01-22 [1] CRAN (R 4.3.3)
+# P curl          5.2.0      2023-12-08 [?] RSPM (R 4.3.0)
+# P data.table  * 1.15.4     2024-03-30 [?] CRAN (R 4.3.3)
+# P datasets    * 4.3.3      2024-02-29 [?] local
+# P digest        0.6.33     2023-07-07 [?] CRAN (R 4.3.1)
+# P dplyr       * 1.1.4      2023-11-17 [?] RSPM (R 4.3.0)
+# P fansi         1.0.6      2023-12-08 [?] RSPM (R 4.3.0)
+# P farver        2.1.1      2022-07-06 [?] CRAN (R 4.3.0)
+# P fastmap       1.1.1      2023-02-24 [?] CRAN (R 4.3.0)
+# P forcats     * 1.0.0      2023-01-29 [?] CRAN (R 4.3.0)
+# P generics      0.1.3      2022-07-05 [?] CRAN (R 4.3.0)
+# P ggbeeswarm  * 0.7.2      2023-04-29 [?] CRAN (R 4.3.3)
+# P ggplot2     * 3.5.0      2024-02-23 [?] CRAN (R 4.3.2)
+# ggstream      0.1.0      2021-05-06 [1] CRAN (R 4.3.3)
+# P ggtext      * 0.1.2      2022-09-16 [?] CRAN (R 4.3.0)
+# P gifski        1.12.0-2   2023-08-12 [?] CRAN (R 4.3.1)
+# P glue        * 1.7.0      2024-01-09 [?] CRAN (R 4.3.2)
+# P graphics    * 4.3.3      2024-02-29 [?] local
+# P grDevices   * 4.3.3      2024-02-29 [?] local
+# P grid          4.3.3      2024-02-29 [?] local
+# P gridtext      0.1.5      2022-09-16 [?] CRAN (R 4.3.0)
+# P gtable        0.3.4      2023-08-21 [?] CRAN (R 4.3.1)
+# P here          1.0.1      2020-12-13 [?] CRAN (R 4.3.0)
+# P hms           1.1.3      2023-03-21 [?] CRAN (R 4.3.0)
+# P htmltools     0.5.7      2023-11-03 [?] RSPM (R 4.3.0)
+# P janitor     * 2.2.0      2023-02-02 [?] CRAN (R 4.3.0)
+# P jsonlite      1.8.8      2023-12-04 [?] RSPM (R 4.3.0)
+# P knitr         1.45       2023-10-30 [?] RSPM (R 4.3.0)
+# P lifecycle     1.0.4      2023-11-07 [?] CRAN (R 4.3.2)
+# P lubridate   * 1.9.3      2023-09-27 [?] RSPM (R 4.3.0)
+# P magick        2.8.3      2024-02-18 [?] CRAN (R 4.3.3)
+# P magrittr      2.0.3      2022-03-30 [?] CRAN (R 4.3.0)
+# P markdown      1.12       2023-12-06 [?] CRAN (R 4.3.2)
+# P MetBrewer   * 0.2.0      2022-03-21 [?] CRAN (R 4.3.1)
+# P methods     * 4.3.3      2024-02-29 [?] local
+# MoMAColors  * 0.0.0.9000 2024-03-31 [1] Github (BlakeRMills/MoMAColors@6f5d75d)
+# P munsell       0.5.0      2018-06-12 [?] CRAN (R 4.3.0)
+# P pacman        0.5.1      2019-03-11 [?] CRAN (R 4.3.0)
+# P pillar        1.9.0      2023-03-22 [?] CRAN (R 4.3.0)
+# P pkgconfig     2.0.3      2019-09-22 [?] CRAN (R 4.3.0)
+# P purrr       * 1.0.2      2023-08-10 [?] CRAN (R 4.3.1)
+# P R6            2.5.1      2021-08-19 [?] CRAN (R 4.3.0)
+# P ragg          1.2.7      2023-12-11 [?] RSPM (R 4.3.0)
+# Rcpp          1.0.12     2024-01-09 [1] CRAN (R 4.3.3)
+# P readr       * 2.1.5      2024-01-10 [?] CRAN (R 4.3.2)
+# renv          1.0.5      2024-02-29 [1] CRAN (R 4.3.3)
+# P repr          1.1.6      2023-01-26 [?] CRAN (R 4.3.0)
+# P rlang         1.1.3      2024-01-10 [?] CRAN (R 4.3.2)
+# P rprojroot     2.0.4      2023-11-05 [?] RSPM (R 4.3.0)
+# P rstudioapi    0.15.0     2023-07-07 [?] CRAN (R 4.3.1)
+# P rsvg          2.6.0      2023-10-08 [?] RSPM (R 4.3.0)
+# P scales      * 1.3.0      2023-11-28 [?] CRAN (R 4.3.2)
+# P sessioninfo   1.2.2      2021-12-06 [?] CRAN (R 4.3.0)
+# P showtext    * 0.9-7      2024-03-02 [?] CRAN (R 4.3.3)
+# P showtextdb  * 3.0        2020-06-04 [?] CRAN (R 4.3.0)
+# P skimr       * 2.1.5      2022-12-23 [?] CRAN (R 4.3.0)
+# P snakecase     0.11.1     2023-08-27 [?] CRAN (R 4.3.1)
+# P stats       * 4.3.3      2024-02-29 [?] local
+# P stringi       1.8.3      2023-12-11 [?] RSPM (R 4.3.0)
+# P stringr     * 1.5.1      2023-11-14 [?] RSPM (R 4.3.0)
+# P svglite       2.1.1      2023-01-10 [?] CRAN (R 4.3.0)
+# P sysfonts    * 0.8.9      2024-03-02 [?] CRAN (R 4.3.3)
+# P systemfonts   1.0.5      2023-10-09 [?] RSPM (R 4.3.0)
+# P textshaping   0.3.6      2021-10-13 [?] CRAN (R 4.3.0)
+# P tibble      * 3.2.1      2023-03-20 [?] CRAN (R 4.3.0)
+# P tidyr       * 1.3.1      2024-01-24 [?] CRAN (R 4.3.2)
+# tidyselect    1.2.1      2024-03-11 [1] CRAN (R 4.3.3)
+# P tidyverse   * 2.0.0      2023-02-22 [?] CRAN (R 4.3.0)
+# P timechange    0.2.0      2023-01-11 [?] CRAN (R 4.3.0)
+# P tools         4.3.3      2024-02-29 [?] local
+# P tzdb          0.4.0      2023-05-12 [?] CRAN (R 4.3.0)
+# P utf8          1.2.4      2023-10-22 [?] CRAN (R 4.3.2)
+# P utils       * 4.3.3      2024-02-29 [?] local
+# P vctrs         0.6.5      2023-12-01 [?] CRAN (R 4.3.2)
+# P vipor         0.4.7      2023-12-18 [?] CRAN (R 4.3.3)
+# P withr         3.0.0      2024-01-16 [?] CRAN (R 4.3.2)
+# P xfun          0.41       2023-11-01 [?] CRAN (R 4.3.2)
+# P xml2          1.3.6      2023-12-04 [?] RSPM (R 4.3.0)
+# 
+# [1] C:/Users/poncest/OneDrive - Bristol Myers Squibb/RStudio/_CHALLENGES/30DayChartChallenge/renv/library/R-4.3/x86_64-w64-mingw32
+# [2] C:/Users/poncest/AppData/Local/R/cache/R/renv/sandbox/R-4.3/x86_64-w64-mingw32/07866f9d
+# 
+# P ── Loaded and on-disk path mismatch.
+# 
+# ───────────────────────────────────────────────────────────────────────────────
+# > 
